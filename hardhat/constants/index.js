dbe6e2a2dd0ce9e29e76b09674763574084a9880
quentin-abei <quentincrto@gmail.com>
2022-03-02 19:40:12 +0100

    nft whitelist mint & public sale

---
+++ hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xE03Ff5dd32beB81e370e83B80962Cf7c669C4ff9";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
